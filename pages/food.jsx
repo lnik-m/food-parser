6b7c897ec01a960c51b93a6c4d75521fe349e638
ksenia312 <ksenia312@users.noreply.github.com>
2022-10-29 18:06:31 +0000

    Prettified Code!

--- pages/food.jsx
+++ pages/food.jsx
@@ -1,13 +1,12 @@
-import styles from '../styles/pages/FoodPage.module.scss';
-
+import styles from "../styles/pages/FoodPage.module.scss";
 
 const Food = () => {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.header}>There will be food</h1>
-            <p>now it is nothing here...</p>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>There will be food</h1>
+      <p>now it is nothing here...</p>
+    </div>
+  );
 };
 
 export default Food;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,23 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import styles from '../styles/pages/Home.module.scss';
+import styles from "../styles/pages/Home.module.scss";
 
-import MainButton from '../components/MainButton/MainButton';
-
+import MainButton from "../components/MainButton/MainButton";
 
 const Home = () => {
-    return (
-        <div className={styles.container}>
-            <h1>It is main page</h1>
+  return (
+    <div className={styles.container}>
+      <h1>It is main page</h1>
 
-            <h3>It is button</h3>
-            <MainButton link={'/food'}>Food page</MainButton>
+      <h3>It is button</h3>
+      <MainButton link={"/food"}>Food page</MainButton>
 
-            <h3>It is using of Link (inside our app)</h3>
-            <Link href="/food">
-                <a>Way to food page</a>
-            </Link>
-        </div>
-    );
+      <h3>It is using of Link (inside our app)</h3>
+      <Link href="/food">
+        <a>Way to food page</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
