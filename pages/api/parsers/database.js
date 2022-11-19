dd8f28015bfe30946d7596225d92f5028d7d0736
michel <mihail.larionow2001@gmail.com>
2022-11-19 19:36:13 +0300

    get and post to fauna db

--- pages/api/parsers/database.js
+++
@@ -1,6 +0,0 @@
-const app = require('express')();
-
-// const faundb = require('faunadb');
-// const client = new faundb.Client({ secret: 'fnAE1nL-jNACTKQAspw5JKeoPazTyzaK3tZuVStm' })
-
-app.listen(5000, () => console.log('API on http://localhost:3000'))
